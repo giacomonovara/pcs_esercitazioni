71ec160d210be0a696fb9bd5f70eaf1681d62f3d
giacomonovara <s322696@studenti.polito.it>
2026-05-18 16:51:44 +0000

    Consegna esercitazione9

---
+++ esercitazione9/build/CMakeFiles/labor9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labor9.dir/link.d"
+  "CMakeFiles/labor9.dir/new_main.cpp.o"
+  "CMakeFiles/labor9.dir/new_main.cpp.o.d"
+  "labor9"
+  "labor9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labor9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
